b953b26f4e878ed3cb6ecc1f7a0d00fbb1e04592
Dario Vera <jdariovera@gmail.com>
2026-01-12 14:02:15 -0300

    Comentario Inicial: estructura del proyecto y config de BD

---
+++ src/main/java/com/gymapp/backend/GymappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gymapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymappBackendApplication.class, args);
+	}
+
+}
